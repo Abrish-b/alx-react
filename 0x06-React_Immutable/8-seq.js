d1b50a4677accae26297bc1d0ca4fbc82f5a2f28
Abrham Bunaro <42895053+Abrish-b@users.noreply.github.com>
2022-08-04 22:31:15 +0300

    quick fix

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,15 +1,21 @@
 import { Seq } from 'immutable';
 
-export default function printBestStudents(grades) {
-	const bestStudents = Seq(grades)
-		.filter((student) => student.score >= 70)
-		.map((student) => ({
-			score: x.score,
-			firstName:
-				student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
-			lastName:
-				student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1),
-		}));
+const printBestStudents = (object) => {
+  const seq = Seq(object);
 
-	console.log(bestStudents.toObject());
-}
+  const getStudent = seq.filter((value) => value.score > 70);
+
+  const student = getStudent.toJS();
+
+  const formatName = (name) => name.charAt(0).toUpperCase() + name.slice(1);
+
+  Object.keys(student).map((key) => {
+    student[key].firstName = formatName(student[key].firstName);
+    student[key].lastName = formatName(student[key].lastName);
+    return student[key];
+  });
+
+  console.log(student);
+};
+
+export default printBestStudents;
